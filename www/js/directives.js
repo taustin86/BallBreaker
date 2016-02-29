408984d26bcdcf476964353406e04a7f85118f71
Tamara Austin <tamara.austin08@gmail.com>
2016-02-29 15:07:45 -0500

    Add js file for directives and add directive to hide tabs bar

---
+++ www/js/directives.js
@@ -0,0 +1,16 @@
+angular.module('starter.directives', [])
+
+.directive('hideTabs', function($rootScope) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attributes) {
+            scope.$watch(attributes.hideTabs, function(value){
+                $rootScope.hideTabs = value;
+            });
+
+            scope.$on('$destroy', function() {
+                $rootScope.hideTabs = false;
+            });
+        }
+    };
+});
