415b35da8c14879c4479e8ee0e9b799fbd874304
Tamara Austin <tamara.austin08@gmail.com>
2016-03-01 22:02:29 -0500

    Initial integration of paper js and sample canvas in play game view

--- www/js/directives.js
+++ www/js/directives.js
@@ -13,4 +13,44 @@ angular.module('starter.directives', [])
             });
         }
     };
+})
+
+.directive('draw', function () {
+    return {
+        restrict: 'AEC',
+        link: function postLink(scope, element, attrs) {
+            var path;
+            var drag = false;
+
+            function mouseUp(event) {
+                //Clear Mouse Drag Flag
+                drag = false;
+            }
+
+            function mouseDrag(event) {
+                if (drag) {
+                    path.add(new paper.Point(event.layerX, event.layerY));
+                    path.smooth();
+                }
+            }
+
+            function mouseDown(event) {
+                //Set  flag to detect mouse drag
+                drag = true;
+                path = new paper.Path();
+                path.strokeColor = 'black';
+                path.add(new paper.Point(event.layerX, event.layerY));
+            }
+
+            function initPaper() {
+                paper.install(window);
+                paper.setup('canvas');
+            }
+
+            element.on('mousedown', mouseDown).on('mouseup', mouseUp).on('mousemove', mouseDrag);
+
+            initPaper();
+
+        }
+    };
 });
