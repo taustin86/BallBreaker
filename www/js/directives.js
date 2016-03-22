69480c80a6b27df15892766d619571bbc9c75286
Tamara Austin <tamara.austin08@gmail.com>
2016-03-22 10:40:37 -0400

    Remove tracker rotation

--- www/js/directives.js
+++ www/js/directives.js
@@ -19,7 +19,7 @@ angular.module('starter.directives', [])
     return {
         restrict: 'AEC',
         link: function postLink(scope, element, attrs) {
-          var trackerRadius = window.innerWidth * .06;
+          var trackerRadius = window.innerWidth * .04;
 
           // Get a reference to the canvas object
           var canvas = document.getElementById('canvas');
@@ -32,21 +32,14 @@ angular.module('starter.directives', [])
             radius: trackerRadius,
             strokeWidth: 2,
             strokeColor: 'white',
-            fillColor: 'white',
-            selected: true
+            fillColor: 'white'
           });
-
-          path.insert(4, new Point(view.center.x - trackerRadius, view.center.y + trackerRadius));
 
           var tool = new Tool();
 
           tool.onMouseDrag = function(event) {
             event.preventDefault();
             path.position = path.position.add(new Point(event.delta.x, event.delta.y));
-          }
-
-          view.onFrame = function(event) {
-            path.rotate(1);
           }
 
           view.onResize = function(event) {
